c9092544f282de2f6385141710e01c0ccfde82e5
yejipractice <twoye1193@ajou.ac.kr>
2021-09-27 17:14:06 +0900

    [Add] 이코테 큰_수의_법칙.py yejipractice

--- yejipractice/이코테/그리디/큰_수의_법칙.py
+++ yejipractice/이코테/그리디/큰_수의_법칙.py
@@ -9,6 +9,6 @@ nums.sort(reverse=True)
 # (가장 큰 수 * K + 두 번째로 큰 수) * a
 
 second = M // (K+1)  # 두 번째로 큰 수 개수
-first = K * second + M % (K+1)  # 가장 큰 수 개수
+first = M-second  # 가장 큰 수 개수
 
 print(nums[0]*first+nums[1]*second)
