3b8ab6b85964a9874850b0cafd1d419ba7fb746d
yejipractice <twoye1193@ajou.ac.kr>
2021-10-06 16:57:46 +0900

    [Add] 이코테 문자열_압축.py yejipractice

--- yejipractice/이코테/구현/문자열_압축.py
+++ yejipractice/이코테/구현/문자열_압축.py
@@ -1,36 +1,24 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 inputs = list(input().rstrip('\n'))
-temp = []
-count = 1
 len_num = len(inputs)
-answer = 0
+
 for i in range(1, len(inputs)//2+1):
-    queue = deque(inputs)
     ans = ''
-    for _ in range(i):
-        temp.append(queue.popleft())
-    while 1:
-        t = []
-        if len(queue) >= i:
-            for _ in range(i):
-                t.append(queue.popleft())
-        if temp == t:
+    temp = inputs[0:i]
+    count = 1
+    for j in range(i, len(inputs), i):
+        if temp == inputs[j:j+i]:
             count += 1
         else:
-            if count == 1:
-                ans += "".join(temp)
-            else:
-                ans += str(count)+"".join(temp)
+            ans += str(count) + "".join(temp) if count >= 2 else "".join(temp)
+            temp = inputs[j:j+i]
             count = 1
-            temp = t
-        if len(queue) < i and t == []:
-            break
-    if queue:
-        ans += "".join(queue)
-    if len_num > len(ans):
-        len_num = len(ans)
+
+    ans += str(count) + "".join(temp) if count >= 2 else "".join(temp)
+    len_num = min(len_num, len(ans))
 
 print(len_num)
+
+# 로직 아이디어는 같지만 구현은 더 간단
