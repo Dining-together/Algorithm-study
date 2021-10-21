6cd1691bdf3110173ced8c2598021d259caab432
yejipractice <twoye1193@ajou.ac.kr>
2021-10-22 01:07:00 +0900

    [Add] 이코테 인구_이동.py yejipractice

--- yejipractice/이코테/DFS&BFS/인구_이동.py
+++ yejipractice/이코테/DFS&BFS/인구_이동.py
@@ -7,47 +7,50 @@ board = []
 for _ in range(n):
     board.append(list(map(int, input().split())))
 
+isMove = False
 
-def check_dfs(visited, x, y):
-    visit_list = []
-    queue = deque([(x, y)])
+
+def check_dfs(x, y, visited):
+    global isMove
+    visitedList = [(x, y)]
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+    summery = board[x][y]
+    count = 1
     while queue:
         i, j = queue.popleft()
-        visited[i][j] = True
-        visit_list.append((i, j))
         cases = [[i+1, j], [i, j+1], [i-1, j], [i, j-1]]
         for case in cases:
             dx, dy = case
-            if dx >= 0 and dy >= 0 and dx < n and dy < n:
+            if dx >= 0 and dy >= 0 and dx < n and dy < n and visited[dx][dy] == False:
                 dif = abs(board[dx][dy] - board[i][j])
                 if dif <= r and dif >= l:
-                    if (dx, dy) not in visit_list:
-                        queue.append((dx, dy))
-    visit_list = list(set(visit_list))
-    if len(visit_list) == 1:
-        return False
-    return visit_list
+                    queue.append((dx, dy))
+                    visited[dx][dy] = True
+                    summery += board[dx][dy]
+                    count += 1
+                    visitedList.append((dx, dy))
+    if count > 1:
+        isMove = True
+        for i, j in visitedList:
+            board[i][j] = summery // count
+
+# 한 반복문에 연합 각 칸의 인구수까지 모두 구하도록 구현해서, 그 부분에서 시간 절약된 듯
 
 
-count = 0
+total_count = 0
+
 while 1:
-    visited = [[False for i in range(n)]for j in range(n)]
-    connected = []
+    visited = [[False]*n for _ in range(n)]
+    isMove = False
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                res = check_dfs(visited, i, j)
-                if res != False:
-                    connected.append(res)
-    if connected == []:
-        break
-    count += 1
-    for c in connected:
-        sum = 0
-        for cx, cy in c:
-            sum += board[cx][cy]
-        score = sum // len(c)
-        for cx, cy in c:
-            board[cx][cy] = score
+                check_dfs(i, j, visited)
 
-print(count)
+    if isMove == False:
+        break
+    total_count += 1
+
+print(total_count)
